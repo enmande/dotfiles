31ec3ccbe2a977f1577fd2146c568031c4371cd7
enmande <3836813+enmande@users.noreply.github.com>
2026-02-14 08:40:45 -0500

    feat(telescope) Show hidden files by default.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
   end,
   keys = {
     -- Keymaps also enable lazy loading on first use
-    { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = '[F]ind [F]iles' },
+    { '<leader>ff', function() require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git'}}) end, desc = '[F]ind [F]iles' },
     { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = '[F]ind via [G]rep' },
     { '<leader>fb', function() require('telescope.builtin').buffers() end, desc = '[F]ind [B]uffers' },
     { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = '[F]ind [H]elp Tags' },
